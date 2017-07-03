6f0d8b2463137be3b26db107543d00e72192ddcf
idpravus <idpravus@gmail.com>
2017-07-03 18:23:00 +0900

    수정 충돌 미반영 부분

--- src/main/java/com/sist/restaurant/ResreplyMapper.java
+++ src/main/java/com/sist/restaurant/ResreplyMapper.java
@@ -10,37 +10,6 @@ import org.apache.ibatis.annotations.Update;
 import com.sist.vo.RestaurantReplyVO;
 
 public interface ResreplyMapper {
-<<<<<<< HEAD
-	 @Select("SELECT COUNT(*) FROM RestaurantReply "
-			 +"WHERE restaurant_id=#{restaurant_id}")
-	  public int replyCount(int restaurant_id);
-	  
-	  @Select("SELECT id,user_id,restaurant_id,reply,score,TO_CHAR(regdate,'YYYY-MM-DD HH24:MI:SS'),group_tab "
-			 +"FROM RestaurantReply WHERE restaurant_id=#{restaurant_id} ORDER BY group_id DESC,group_step ASC")
-	  public List<RestaurantReplyVO> replyListData(int restaurant_id);
-	  
-	  @Insert("INSERT INTO RestaurantReply VALUES("
-			 +"dr_no_seq.nextval,#{user_id},#{restaurant_id},"
-			 +"#{reply},#{score},SYSDATE,(SELECT NVL(MAX(group_id)+1,1) FROM RestaurantReply),"
-			 +"0,0,0,0)")
-	  public void replyNewInsert(RestaurantReplyVO vo);
-	  
-	  @Update("UPDATE RestaurantReply SET "
-			 +"reply=#{reply} "
-			 +"WHERE id=#{id}")
-	  public void replyUpdate(RestaurantReplyVO vo);
-	  // ���? => ���?
-	  @Select("SELECT group_id,group_step,group_tab "
-			 +"FROM RestaurantReply "
-			 +"WHERE id=#{id}")
-	  public RestaurantReplyVO replyParentInfoData(int id);
-	  
-	  @Update("UPDATE RestaurantReply SET "
-			 +"group_step=group_step+1 "
-			 +"WHERE group_id=#{group_id} "
-			 +"AND group_step>#{group_step}")
-	  public void replyStepIncrement(RestaurantReplyVO vo);
-=======
 /*	@Select("SELECT id, user_id, restaurant_id, reply, score, regdate, img_ori, img_new "
 			+ "from restaurant_reply where restaurant_id=#{restaurantId} ORDER BY id")*/
 	@Select("SELECT * FROM restaurant_reply where restaurant_id=#{restaurantId} ORDER BY id")
@@ -54,7 +23,6 @@ public interface ResreplyMapper {
 	public void insert(RestaurantReplyVO vo);
 
 }
->>>>>>> 7e62c2c7d94178ad2b6aabd59aa5792dd07f1808
 
 	  @Insert("INSERT INTO RestaurantReply VALUES("
 				 +"dr_no_seq.nextval,#{user_id},#{restaurant_id},"
